bf768c93606b8622727e84ea94fb8c9f25dce63f
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-06-18 19:37:57 +0700

    Emacs: Optimize emacs startup

--- emacs2.el
+++ emacs2.el
@@ -1,1 +1,30 @@
 ;; emacs.el --- Emacs configuration file -*- lexical-binding: t -*-
+
+;;; Optimization
+;; set default value of gc-cons and file-name-handler
+(defvar default-gc-cons-threshold gc-cons-threshold)
+(defvar default-gc-cons-percentage gc-cons-percentage)
+(defvar default-file-name-handler-alist file-name-handler-alist)
+
+;; change the default value
+(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.6)
+(setq file-name-handler-alist nil)
+
+;; restore default value after loading init files
+(add-hook 'emacs-startup-hook
+          #'(lambda ()
+              (setq gc-cons-threshold default-gc-cons-threshold
+                    gc-cons-percentage default-gc-cons-percentage
+                    file-name-handler-alist default-file-name-handler-alist)))
+
+;; change gc-cons value when entering minibuffer and restore when exit
+(add-hook 'minibuffer-setup-hook
+          #'(lambda ()
+              (setq gc-cons-threshold most-positive-fixnum)))
+(add-hook 'minibuffer-exit-hook
+          #'(lambda ()
+              (garbage-collect)
+              (setq gc-cons-threshold default-gc-cons-threshold)))
+
+
