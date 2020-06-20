ca8daa8a2da60332b1d04895d8d62e5d63e5197a
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-06-21 05:55:09 +0700

    emacs: Update configuration file location

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-ln -s /home/$USER/myFiles/emacs.el /home/$USER/.emacs.d/init.el
+# emacs
+ln -s /home/$USER/myFiles/emacs/early-init.el /home/$USER/.config/emacs/early-init.el
+#ln -s /home/$USER/myFiles/emacs/init.el /home/$USER/.config/emacs/init.el
+
 ln -s /home/$USER/myFiles/zshrc /home/$USER/.zshrc
 ln -s /home/$USER/myFiles/tmux.conf /home/$USER/.tmux.conf
