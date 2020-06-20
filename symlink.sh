7a393a52c49a680e5e79d102495567844d5e60a1
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-06-20 14:33:41 +0700

    Rename files and path

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s /home/$USER/Setting/emacs.el /home/$USER/.emacs.d/init.el
-ln -s /home/$USER/Setting/zsh /home/$USER/.zshrc
-ln -s /home/$USER/Setting/tmux /home/$USER/.tmux.conf
+ln -s /home/$USER/myFiles/emacs.el /home/$USER/.emacs.d/init.el
+ln -s /home/$USER/myFiles/zshrc /home/$USER/.zshrc
+ln -s /home/$USER/myFiles/tmux.conf /home/$USER/.tmux.conf
