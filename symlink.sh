ce1420774d33711c5dada0c930178d2bff5cede6
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-06-21 10:09:50 +0700

    emacs: Rename emacs.el to init.el

--- symlink.sh
+++ symlink.sh
@@ -2,7 +2,7 @@
 
 # emacs
 ln -s /home/$USER/myFiles/emacs/early-init.el /home/$USER/.config/emacs/early-init.el
-#ln -s /home/$USER/myFiles/emacs/init.el /home/$USER/.config/emacs/init.el
+ln -s /home/$USER/myFiles/emacs/init.el /home/$USER/.config/emacs/init.el
 
 ln -s /home/$USER/myFiles/zshrc /home/$USER/.zshrc
 ln -s /home/$USER/myFiles/tmux.conf /home/$USER/.tmux.conf
