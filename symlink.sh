01cbe96f6d85855c63b38ae577da34c9328e1a2b
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-09-21 11:23:04 +0700

    make emacs directory before create the symlink2

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # emacs
-ln -s /home/$USER/myFiles/emacs/early-init.el /home/$USER/.config/emacs/early-init.el
-ln -s /home/$USER/myFiles/emacs/init.el /home/$USER/.config/emacs/init.el
+mkdir -p /home/$USER/.config/emacs
+ln -s ./emacs/early-init.el /home/$USER/.config/emacs/early-init.el
+ln -s ./emacs/init.el /home/$USER/.config/emacs/init.el
 
 ln -s /home/$USER/myFiles/zshrc /home/$USER/.zshrc
 ln -s /home/$USER/myFiles/tmux.conf /home/$USER/.tmux.conf
