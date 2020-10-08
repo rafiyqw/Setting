b281fac157c418b47c6c76121e2093c66a5fe4b6
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-10-08 15:03:58 +0700

    Clean-up config

--- setup.sh
+++ setup.sh
@@ -8,17 +8,21 @@ ln -sfv $PWD/tmux.conf /home/$USER/.tmux.conf
 rm -rf emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
 touch emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
 chmod +x emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
-echo "#!/bin/sh" > emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
-echo "rm -rf ~/.emacs.d" >> emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
 # vanilla emacs
+echo "#!/bin/sh" > emacs/vanilla.sh
+echo "rm -rf ~/.emacs.d" >> emacs/vanilla.sh
 echo "ln -sfv $PWD/emacs ~/.emacs.d" >> emacs/vanilla.sh
 # spacemacs
+echo "#!/bin/sh" > spacemacs/spacemacs.sh
+echo "rm -rf ~/.emacs.d" >> spacemacs/spacemacs.sh
 echo "ln -sfv ~/.config/spacemacs ~/.emacs.d" >> spacemacs/spacemacs.sh
 echo "ln -sfv $PWD/spacemacs/spacemacs-master ~/.spacemacs" >> spacemacs/spacemacs.sh
 # spacemacs-develop
-echo "ln -sfv ~/.config/spacemacs-dev ~/.emacs.d" >> spacemacs/spacemacs.sh
-#echo "ln -sfv $PWD/spacemacs/spacemacs-dev ~/.spacemacs" >> spacemacs/spacemacs.sh
+echo "#!/bin/sh" > spacemacs/spacemacs-dev.sh
+echo "rm -rf ~/.emacs.d" >> spacemacs/spacemacs-dev.sh
+echo "ln -sfv ~/.config/spacemacs-dev ~/.emacs.d" >> spacemacs/spacemacs-dev.sh
+echo "ln -sfv $PWD/spacemacs/spacemacs-dev ~/.spacemacs" >> spacemacs/spacemacs-dev.sh
 
-git clone https://github.com/syl20bnr/spacemacs ~/.config/spacemacs
-git clone -b develop https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-dev
+#git clone https://github.com/syl20bnr/spacemacs ~/.config/spacemacs
+#git clone -b develop https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-dev
 
--- zshrc
+++ zshrc
@@ -67,21 +67,3 @@ alias ll='ls -AlFh'
 alias la='ls -A'
 alias l='ls -CF'
 
-# more aliases
-alias youtube-dl='python3 -m /home/$USER/myFiles/submodules/youtube-dl/youtube-dl'
-alias speedtest='python3 /home/$USER/myFiles/submodules/speedtest-cli/speedtest.py'
-
-# emacs vterm
-vterm_printf(){
-    if [ -n "$TMUX" ]; then
-        # Tell tmux to pass the escape sequences through
-        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-        printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-    elif [ "${TERM%%-*}" = "screen" ]; then
-        # GNU screen (screen, screen-256color, screen-256color-bce)
-        printf "\eP\e]%s\007\e\\" "$1"
-    else
-        printf "\e]%s\e\\" "$1"
-    fi
-}
-
