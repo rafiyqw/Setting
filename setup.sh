d447d971e82fd60c389d846c04f555e082f91faf
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-11-30 07:01:04 +0700

    spacemacs: Remove spacemacs develop branch

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ ln -sfv $PWD/vimrc /home/$USER/.vimrc
 ln -sfv $PWD/tmux.conf /home/$USER/.tmux.conf
 
 # emacs & spacemacs
-rm -rf emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
-touch emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
-chmod +x emacs/vanilla.sh spacemacs/spacemacs.sh spacemacs/spacemacs-dev.sh
+rm -rf emacs/vanilla.sh spacemacs/spacemacs.sh 
+touch emacs/vanilla.sh spacemacs/spacemacs.sh 
+chmod +x emacs/vanilla.sh spacemacs/spacemacs.sh 
 # vanilla emacs
 echo "#!/bin/sh" > emacs/vanilla.sh
 echo "rm -rf ~/.emacs.d" >> emacs/vanilla.sh
@@ -17,12 +17,6 @@ echo "#!/bin/sh" > spacemacs/spacemacs.sh
 echo "rm -rf ~/.emacs.d" >> spacemacs/spacemacs.sh
 echo "ln -sfv ~/.config/spacemacs ~/.emacs.d" >> spacemacs/spacemacs.sh
 echo "ln -sfv $PWD/spacemacs/spacemacs ~/.spacemacs" >> spacemacs/spacemacs.sh
-# spacemacs-develop
-echo "#!/bin/sh" > spacemacs/spacemacs-dev.sh
-echo "rm -rf ~/.emacs.d" >> spacemacs/spacemacs-dev.sh
-echo "ln -sfv ~/.config/spacemacs-dev ~/.emacs.d" >> spacemacs/spacemacs-dev.sh
-echo "ln -sfv $PWD/spacemacs/spacemacs-dev ~/.spacemacs" >> spacemacs/spacemacs-dev.sh
 
 git clone https://github.com/syl20bnr/spacemacs ~/.config/spacemacs
-git clone -b develop https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-dev
 
