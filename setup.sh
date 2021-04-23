a504dcc996544d4f86d9537adcb161596773cf89
rafiyq <rafiyq@protonmail.com>
2021-04-23 08:54:47 +0700

    organizing file

--- setup.sh
+++ setup.sh
@@ -1,48 +1,47 @@
 #!/bin/sh
 
-ln -sfv $PWD/vim/vimrc ~/.vimrc
-ln -sfv $PWD/tmux/tmux.conf ~/.tmux.conf
-ln -sfv $PWD/bin ~/.local/bin
-ls -sfv $PWD/apps ~/.local/share/applications
+ln -sfv $PWD/vim/vimrc $HOME/.vimrc
+ln -sfv $PWD/tmux/tmux.conf $HOME/.tmux.conf
+ln -sfv $PWD/bin $HOME/.local/
+ln -sfv $PWD/applications $HOME/.local/share/
 
 # zsh
 [ ! -f $PWD/zsh/zshrc ] && wget -O $PWD/zsh/zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 ln -sfv $PWD/zsh/zshrc $HOME/.zshrc
 ln -sfv $PWD/zsh/zshrc.local $HOME/.zshrc.local
 
-# emacs & spacemacs
-rm -rf emacs/emacs.sh emacs/spacemacs-master.sh emacs/spacemacs-develop.sh
-touch emacs/emacs.sh emacs/spacemacs-master.sh emacs/spacemacs-develop.sh
-chmod +x emacs/emacs.sh emacs/spacemacs-master.sh emacs/spacemacs-develop.sh
-
 # emacs
-tee -a emacs/emacs.sh << END
+tee $HOME/.local/bin/start-emacs << END
 #!/bin/sh
 rm -rf ~/.emacs.d
 mkdir -pv ~/.config/emacs
 ln -sfv $PWD/emacs/early-init.el ~/.config/emacs/
 ln -sfv $PWD/emacs/init.el ~/.config/emacs/
 ln -sfv ~/.config/emacs ~/.emacs.d
+/usr/bin/emacs
 END
-ln -sfv $PWD/emacs/emacs.sh ~/.local/bin/
+chmod +x $HOME/.local/bin/start-emacs
 
 # spacemacs-master
-tee -a emacs/spacemacs-master.sh << END
+tee $HOME/.local/bin/start-spacemacs-master << END
 #!/bin/sh
 rm -rf ~/.emacs.d
 ln -sfv ~/.config/spacemacs-master ~/.emacs.d
 ln -sfv $PWD/emacs/spacemacs-master.el ~/.spacemacs
+/usr/bin/emacs
 END
-ln -sfv $PWD/emacs/spacemacs-master.sh ~/.local/bin/
+chmod +x $HOME/.local/bin/start-spacemacs-master
 
 # spacemacs-develop
-tee -a emacs/spacemacs-develop.sh << END
+tee $HOME/.local/bin/start-spacemacs-develop << END
 #!/bin/sh
 rm -rf ~/.emacs.d
 ln -sfv ~/.config/spacemacs-develop ~/.emacs.d
 ln -sfv $PWD/emacs/spacemacs-develop.el ~/.spacemacs
+/usr/bin/emacs
 END
-ln -sfv $PWD/emacs/spacemacs-develop.sh ~/.local/bin/
+chmod +x $HOME/.local/bin/start-spacemacs-develop
 
-[ ! -d "~/.config/spacemacs-master" ] && git clone -b master https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-master
-[ ! -d "~/.config/spacemacs-develop" ] && git clone -b develop https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-develop
+# clone spacemacs
+[ ! -d $HOME/.config/spacemacs-master ] && git clone -b master https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-master
+[ ! -d $HOME/.config/spacemacs-develop ] && git clone -b develop https://github.com/syl20bnr/spacemacs ~/.config/spacemacs-develop
