13eb73eaba54d71455cb7c5694ee16ed464538d2
rafiyq <rafiyq@protonmail.com>
2021-07-20 19:23:13 +0700

    nvim: configure lsp and autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,19 @@
 -------------------- HELPERS -------------------------------
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
-local opt = vim.opt  -- to set options
+local cmd = vim.cmd            -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn   	       -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g     	       -- a table to access global variables
+local opt = vim.opt 	       -- to set options
+
 -------------------- PLUGINS -------------------------------
-require('plugins')   -- install plugins
+require('plugins')   	       -- install plugins
+
 -------------------- OPTIONS -------------------------------
+cmd 'colorscheme gruvbox'     -- Sierra colorscheme
+opt.expandtab = true           -- Use spaces instead of tabs
+opt.ignorecase = true          -- Ignore case
+opt.laststatus = 1             -- show status if two windows
+opt.number = true              -- show line number
+opt.completeopt = "menuone,noselect" 	-- enable autocompletion
+
 -------------------- MAPPINGS ------------------------------
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,14 +10,14 @@ if fn.empty(fn.glob(install_path)) > 0 then
   execute 'packadd packer.nvim'
 end
 
---vim.cmd [[packadd packer.nvim]]
--- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
---vim._update_package_paths()
---
---return require('packer').startup(function(use)
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'  -- Packer can manage itself
-  use 'neovim/nvim-lspconfig'   -- built-in language server client.
+  use 'neovim/nvim-lspconfig'   -- Built-in language server client.
+  use 'hrsh7th/nvim-compe'	-- Auto completion Lua plugin for nvim.
+  use 'L3MON4D3/LuaSnip'	-- Snippet Engine written in Lua.
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'morhetz/gruvbox'         -- Gruvbox colorscheme
+
 end)
 
 --------------------- LSP ----------------------------------
@@ -62,8 +62,96 @@ local servers = { "pyright", "rust_analyzer", "tsserver" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
+    capabilities = capabilities,
     flags = {
       debounce_text_changes = 150,
     }
   }
 end
+
+
+-------------------- Compe  --------------------------------
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  resolve_timeout = 800;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+    luasnip = true;
+  };
+}
+
+-- Utility functions for compe and luasnip
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+  local col = vim.fn.col '.' - 1
+  if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
+    return true
+  else
+    return false
+  end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menu
+--- jump to prev/next snippet's placeholder
+local luasnip = require 'luasnip'
+
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-n>'
+  elseif luasnip.expand_or_jumpable() then
+    return t '<Plug>luasnip-expand-or-jump'
+  elseif check_back_space() then
+    return t '<Tab>'
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-p>'
+  elseif luasnip.jumpable(-1) then
+    return t '<Plug>luasnip-jump-prev'
+  else
+    return t '<S-Tab>'
+  end
+end
+
+-- Map tab to the above tab complete functions
+vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+
+-- Map compe confirm and complete functions
+vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
+vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
